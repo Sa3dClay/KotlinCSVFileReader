c3db93c27154e560aeb60d07154cc4a6a0aed32d
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2019-11-29 20:54:03 +0200

    Create CSVReader.kt

---
+++ CSVReader.kt
@@ -0,0 +1,43 @@
+package myModel
+
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
+
+class CSVReader {
+
+    fun readCSV(path: String): Array<String> {
+        var data = arrayOf<String>()
+        var fileReader: BufferedReader? = null
+
+        try {
+            var line: String?
+            fileReader = BufferedReader(FileReader(path))
+            // Read CSV header
+            fileReader.readLine()
+            // Read the file line by line starting from the second line
+            line = fileReader.readLine()
+
+            while (line != null) {
+                val tokens = line.split(",")
+
+                if (tokens.isNotEmpty()) {
+                    println(tokens)
+                    data = data.plus(tokens)
+                }
+                line = fileReader.readLine()
+            }
+        } catch (e: Exception) {
+            println("Reading CSV Error!: $e")
+        } finally {
+            try {
+                fileReader?.close()
+            } catch (e: IOException) {
+                println("Closing fileReader Error!: $e")
+            }
+        }
+
+        return data
+    }
+
+}
